0b1a92ffedb605b4568206da4b89b61d5a0f1b72
Anuj <anuj992393@gmail.com>
2023-02-17 22:26:03 +0530

    removed validator module

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const validator = require('validator');
 
 // DESIGNING USER SCHEMA
 const userSchema = new mongoose.Schema({
